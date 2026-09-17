01197a6aaab2f5f18fad9cec359911740bfe9bc9
Paytabyte <122190094+SchmidtyPlays@users.noreply.github.com>
2026-09-17 16:36:51 -0700

    Upload initial files
    These are all of the files previously locally stored

---
+++ script.js
@@ -0,0 +1,41 @@
+const availableTexts = [
+    "security",
+    "stability",
+    "privacy",
+    "connection",
+    "safety",
+    "protection"
+];
+
+var text = "";
+var i = 0;
+var j = 0;
+
+async function changeText() {
+    for (i; i <= availableTexts.length; i++) {
+        if (i == availableTexts.length) {
+            i = 0;
+        }
+        text = availableTexts[i];
+
+        while (j < text.length) {
+            document.getElementById("changingText").innerHTML += text.charAt(j);
+            j++;
+            await sleep(50);
+        }
+
+        await sleep(3000);
+
+        while (j > 0) {
+            document.getElementById("changingText").innerHTML = document.getElementById("changingText").innerHTML.slice(0, -1);
+            j--;
+            await sleep(50);
+        }
+    }
+}
+
+async function sleep(ms) {
+  await new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+changeText();
